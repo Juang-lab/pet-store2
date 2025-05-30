1079662ca17d2bea751343e0bb1f392fd57b456f
Juan <jugarc9427@gmail.com>
2025-05-29 21:54:27 -0500

    integration bootstrap

---
+++ src/config/database.php
@@ -0,0 +1,32 @@
+<?php
+     $host        ="aws-0-us-east-1.pooler.supabase.com";
+     $port        ="6543";
+     $dbname      ="postgres";
+     $user        ="postgres.beiffshkcrbekdbyfsiu";
+     $password    ="unicesmag@@";
+    /*
+    $host        ="localhost";
+    $port        ="5432";
+    $dbname      ="petstore";
+    $user        ="postgres";
+    $password    ="postgres";
+*/
+    $data_connection="
+        host=$host
+        port=$port
+        dbname=$dbname
+        user=$user
+        password=$password
+    ";
+    
+    $conn = pg_connect($data_connection);
+
+    if(!$conn){
+        echo "Connection error";
+
+    }else{
+        echo "Success !!!";
+    }
+
+    //pg_close($conn);
+?>
